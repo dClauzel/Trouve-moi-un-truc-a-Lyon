135768393319170eafeff4f110af3d07913d01b8
Damien Clauzel <Damien@Clauzel.eu>
2013-05-18 16:47:26 +0200

    fix

--- Silos verre/trouver-silos-verre.php
+++ Silos verre/trouver-silos-verre.php
@@ -2,7 +2,7 @@
 
 require_once '../fonctions.php'; // fonctions génériques (calcul de distance, etc)
 require_once '../clés-api.php'; // clés pour authentification sur les API
-require_once 'GLfonctions.php'; // fonctions spécifiques aux données du GL
+require_once '../GLfonctions.php'; // fonctions spécifiques aux données du GL
 
 /* récupération des données du GL
 Idéalement devrait utiliser https://github.com/geonef/php5-gdal/ (ne fonctionne pas) ou swig de la lib gdal pour créer un module php (erreur de compilation), mais faute de mieux on passe par un appel à un outil externe.
@@ -16,14 +16,14 @@ unlink($tmpfile);
 require_once '../Patrons html/haut.php';
 
 // tri des silos verre selon la plus petite distance par rapport à la position de l'utilisateur
-usort($Donnees["features"], "GLCompareDistanceSiloVerre");
+usort($Donnees["features"], "GL_CompareDistance");
 
 // liste les 3 résultats les plus proches et les affiche avec un marqueur
 
 $NbResultats=0;
 $i=0;
 while($NbResultats<3) {
-	echo "document.getElementById('resultat$NbResultats').innerHTML = '". GLAfficheSiloVerre($Donnees["features"][$i]). "';\n";
+	echo "document.getElementById('resultat$NbResultats').innerHTML = '". GL_SiloVerre_Affiche($Donnees["features"][$i]). "';\n";
 	echo "var marker = new google.maps.Marker({
 		position: new google.maps.LatLng(" .$Donnees["features"][$i]["geometry"]["coordinates"][0]. "," .$Donnees["features"][$i]["geometry"]["coordinates"][1]. "),
 		map: map,

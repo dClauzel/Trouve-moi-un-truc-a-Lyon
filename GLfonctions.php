56eab3400ba15958acbe924d8ed1fbe1b38ba405
Damien Clauzel <Damien@Clauzel.eu>
2013-05-18 16:43:17 +0200

    ajout de la recherche de fontaines d'eau potable

--- GLfonctions.php
+++ GLfonctions.php
@@ -2,10 +2,10 @@
 
 require_once '../fonctions.php'; // fonctions génériques
 
-/* fonction de comparaison de 2 stations, pour usort()
+/* fonction de comparaison de 2 éléments, pour usort()
  * la comparaison se fait par rapport à la distance de l'utilisateur
  */
-function GLCompareDistanceSiloVerre($point1, $point2) {
+function GL_CompareDistance($point1, $point2) {
 
 	global $latitude, $longitude;
 	$distance1 = distance($latitude, $longitude, $point1["geometry"]['coordinates'][0], $point1["geometry"]['coordinates'][1]);
@@ -17,8 +17,28 @@ function GLCompareDistanceSiloVerre($point1, $point2) {
 	return ($distance1 < $distance2) ? -1 : 1;
 }
 
+// affiche en liste les données d'une borne fontaine
+function GL_BorneFontaine_Affiche($station) {
+	global $latitude, $longitude;
+	$res = "<ul>";
+	$res .= "<li>type : " . $station["type"];
+	$res .= "<li>gml_id : " . $station["properties"]["gml_id"];
+	$res .= "<li>nom : " . $station["properties"]["nom"];
+	$res .= "<li>gestionnaire : " . $station["properties"]["gestionnaire"];
+	$res .= "<li>anneepose : " . $station["properties"]["anneepose"];
+	$res .= "<li>gid : " . $station["properties"]["gid"];
+	$res .= "<li>latitude : " . $station["geometry"]["coordinates"][0];
+	$res .= "<li>longitude : " . $station["geometry"]["coordinates"][1];
+	$res .= "<ul>";
+	$res .= "\t<li>distance : " . distance($latitude,$longitude,$station["geometry"]["coordinates"][0],$station["geometry"]["coordinates"][1]);
+	$res .= "</ul>";
+	$res .= "</ul>";
+
+	return $res;
+}
+
 // affiche en liste les données sur un silo verre
-function GLAfficheSiloVerre($station) {
+function GL_SiloVerre_Affiche($station) {
 	global $latitude, $longitude;
 	$res = "<ul>";
 	$res .= "<li>type : " . $station["type"];
